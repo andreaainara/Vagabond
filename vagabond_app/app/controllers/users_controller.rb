6adbacc2eda555a2a9901e73582a92abecc60eee
ruhsam <ruhsam@gmail.com>
2016-05-05 15:35:14 -0700

    Rough user controller new_create_show first run

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+   def index
+      @users = User.all
+      render :index
+   end
+
+####Stores new account in db
+   def new
+      @user = User.new
+      render :new
+   end
+
+
+####Sending user info to city-user post join table#####
+   def create
+      user_params = params.require( :user).permit( :first_name, :last_name, :created_at, :current_city)
+      @user = User.create(user_params)
+   end
+####Show profile page####
+   def show
+      @user = User.find_by_id(params[:id])
+      render :show
+   end
 end
