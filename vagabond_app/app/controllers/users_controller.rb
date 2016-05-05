e88785aec9062fdf4a7052398651b27b33a24de8
ruhsam <ruhsam@gmail.com>
2016-05-05 16:01:37 -0700

    user controller updated to avoid future merge conflict

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -1,22 +1,12 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
    def index
       @users = User.all
       render :index
    end
 
-####Stores new account in db // sign up page?
-   def new
-      @user = User.new
-      render :new
-   end
 
 
-####Sending user info to city-user post join table#####
-   def create
-      user_params = params.require( :user).permit( :first_name, :last_name, :created_at, :current_city)
-      @user = User.create(user_params)
-   end
+
 
 ####Show profile page####
    def show
