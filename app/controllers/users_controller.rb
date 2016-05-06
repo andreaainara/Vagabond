5c0daa0fb487f4563a984933695dd71c9904c6fd
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-06 11:04:33 -0700

    Changed file structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
-   before_action :logged_in?, only: [:show]
+  before_action :logged_in?, only: [:show]
+  
+  def index
+    render :index
+  end
 
-   def show
-      @user = User.find(params[:id])
-      render :show
-   end
-   
-   def edit
-      user_id = params[:id]
-      @user = User.find_by_id(user_id)
-      render :edit
-   end
+  def new
+     @user = User.new
+     render :new
+  end
 
-   def update
-      user_id = params[:id]
-      user = User.find_by_id(user_id)
-      user_params = params.require(:user).permit(:name, :description)
-      user.update_attributes(user_params)
-      redirect_to user_path(user)
-   end
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :current_city, :email, :password)
+    @user = User.create(user_params)
+
+    login(@user)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    user_id = params[:id]
+     @user = User.find_by_id(user_id)
+     render :edit
+  end
+
+  def update
+     user_id = params[:id]
+     user = User.find_by_id(user_id)
+     user_params = params.require(:user).permit(:name, :description)
+     user.update_attributes(user_params)
+    redirect_to user_path(user)
+  end
 end
