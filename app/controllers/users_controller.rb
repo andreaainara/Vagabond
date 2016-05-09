9f1417adafa4be07f56b1cf9e134f34092acda6e
andreaainara <andrea.ainara10@gmail.com>
2016-05-09 15:31:37 -0700

    forgot to add a success not to account sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.save
       login(@user)
+      flash[:success] = "Welcome to the Vagabond Community!"
       redirect_to root_path
     else
       render :new
