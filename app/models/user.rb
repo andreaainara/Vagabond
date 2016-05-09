c7b9576eb2de854c601269a97d18684f2a849c76
andreaainara <andrea.ainara10@gmail.com>
2016-05-09 13:55:26 -0700

    added form validations to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :current_city, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence:   true,
+            format:     { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            length:     { maximum: 255 }
+  validates :password_digest, presence: true, length: { minimum: 6 }
+
   has_secure_password
   has_many :posts
 
